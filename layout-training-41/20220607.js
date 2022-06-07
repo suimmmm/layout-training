8f76c51874c4d609f06603eb176c07d02ae5b6a0
suim <spdlqjtn159@gmail.com>
2022-06-07 10:26:21 +0900

    작업중

---
+++ layout-training-41/20220607.js
@@ -0,0 +1,7 @@
+const slideItem = document.querySelectorAll('#slide-item > #item')
+const btnLeft = document.getElementById('left')
+const btnRight = document.getElementById('right')
+console.log(slideItem);
+console.log(btnLeft);
+console.log(btnRight);
+
